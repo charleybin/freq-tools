9aa311f032939715ad7b49a9965a2ca2c9e697d5
charleybin <charleybin@gmail.com>
2023-02-01 13:15:55 +0800

    Update inst_docker.sh

--- inst_docker.sh
+++ inst_docker.sh
@@ -1,5 +1,11 @@
-sudo apt update
-sudo apt install -y docker.io
+##ubuntu
+#sudo apt update
+#sudo apt install -y docker.io
+## centos
+#sudo yum install -y docker.io
+
+curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo groupadd docker
