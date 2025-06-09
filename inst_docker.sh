c81bc2836bb7badd1d46c22500ad9c5ad1e0e118
bxy <shubin@vrvmail.com.cn>
2025-06-09 15:12:30 +0800

    support pull docker images from insecure docker-hub

--- inst_docker.sh
+++ inst_docker.sh
@@ -13,3 +13,17 @@ sudo gpasswd -a $USER docker
 newgrp docker
 docker ps -a
 
+
+[add insecure-registries]
+/etc/docker/daemon.json
+{
+  "insecure-registries": ["aa.bb.cc.dd"]
+}
+
+openssl s_client -connect aa.bb.cc.dd:443 -showcerts </dev/null 2>/dev/null | openssl x509 -outform PEM > registry.crt
+
+sudo cp registry.crt /usr/local/share/ca-certificates/
+sudo update-ca-certificates
+
+docker login aa.bb.cc.dd
+docker pull aa.bb.cc.dd/xxx
